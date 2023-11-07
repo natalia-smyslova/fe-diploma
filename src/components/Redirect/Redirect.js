caec8015b4a3ff6753dcee40439cdb1964626ba2
Smyslova Natalia <madlynx@lenta.ru>
2023-11-07 23:47:00 +0300

    reviews

--- src/components/Redirect/Redirect.js
+++
@@ -1,5 +0,0 @@
-// import  React  from 'react';
-
-function Redirect(){};
-
-export default Redirect;
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import CarouselSlider from './Slider/CarouselSlider';
-// import Review from './Reviews/Review';
-// import userReviews from './Reviews/users/userReviews';
 
 
 import styles from './Reviews.module.scss';
@@ -11,17 +9,6 @@ function Reviews() {
    return (
       <div className={styles.reviews} id="reviews">
          <span className={styles.reviews__title}>отзывы</span>
-         {/* <div className={styles.reviews__cards}>
-            {userReviews.map((review) => (
-               <Review
-                  key={review.id}
-                  img={review.img}
-                  name={review.name}
-                  head={review.head}
-                  text={review.text}
-               />
-            ))}
-         </div> */}
          <CarouselSlider />
       </div>
    );
